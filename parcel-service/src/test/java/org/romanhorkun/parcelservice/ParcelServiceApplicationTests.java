bdbb6756c7bdca88c16b27ef01c6d394ca94e09f
romgor <romanhorkoon@gmail.com>
2024-03-26 21:58:40 +0200

    Add parcel, department, delivery, notification microservices

---
+++ parcel-service/src/test/java/org/romanhorkun/parcelservice/ParcelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.romanhorkun.parcelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
